be6bd119e37047e1f21f535314befedca7734e96
Akshay PS <psakshay2022@gmail.com>
2025-03-16 18:08:33 +0530

    research using url

--- src/modules/reviews/components/research-form.tsx
+++ src/modules/reviews/components/research-form.tsx
@@ -1,4 +1,8 @@
-import { Button } from "@/components/ui/button"
+import { useNavigate } from "@tanstack/react-router"
+import { AtSign, Search } from "lucide-react"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button.tsx"
 import {
   Form,
   FormControl,
@@ -6,25 +10,23 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { PhoneInput } from "@/components/ui/phone-input"
+} from "@/components/ui/form.tsx"
+import { Input } from "@/components/ui/input.tsx"
+import { PhoneInput } from "@/components/ui/phone-input.tsx"
 import {
   ResearchFormData,
   researchFormResolver,
 } from "@/lib/forms/research.form"
-import { Route } from "@/routes/_private/index"
-import { useNavigate } from "@tanstack/react-router"
-import { AtSign, Search } from "lucide-react"
-import { useForm } from "react-hook-form"
+import { Route } from "@/routes/_private/index.tsx"
 
 export function ResearchForm() {
+  const { email, phone } = Route.useSearch()
   const navigate = useNavigate({ from: Route.fullPath })
   const form = useForm<ResearchFormData>({
     resolver: researchFormResolver,
     defaultValues: {
-      email: undefined,
-      phone: undefined,
+      email,
+      phone,
     },
   })
 
